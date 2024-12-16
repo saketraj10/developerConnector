25604a8ca8d5b32eb615c345ae98bdb4bb3b49ed
Saket Raj <saketraj01227@gmail.com>
2024-12-16 14:41:53 +0530

    explore get, post, delete

--- Readme.md
+++ Readme.md
@@ -9,3 +9,12 @@
 - What are dependencies
 - What is the use of "-g" while npm install
 - Difference between caret and tilde ( ^ vs ~ )
+
+- Initialize git
+- .gitignore
+- Create a remote repo on github
+- Push all code to remote origin
+- Play with routes and route extensions ex. /hello, /, /hello/2, /xyz
+- Order of the routes matter a lot
+- Install Postman app and make a workspace/collection > test API call
+- Write logic to handle GET, POST, PATCH, DELETE API calls and test them on Postman
--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello",(req, res) =>{
-  res.send("hello hello hello!");
+// This will only from GET method
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Saket", lastName: "Raj"});
 });
 
+app.post("/user", (req, res) => {
+  // saving data to DB
+  res.send("Data Successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  // saving data to DB
+  res.send("Deleted Successfully");
+});
+
+// this will match all the HTTP method API calls to /test
 app.use("/test",(req, res) =>{
   res.send("Hello from the server!");
 });
+
+
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...")
