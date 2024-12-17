05026e1b312f68db8f8047da2d54e9cc3e075cab
Saket Raj <saketraj01227@gmail.com>
2024-12-17 14:05:25 +0530

    writing-auth-middleware

--- Readme.md
+++ Readme.md
@@ -27,3 +27,9 @@
 - next()
 - next function and errors along with res.send()
 - app.use("/route", rH, [rH2, rH3], rH4, rH5);
+- What is Middleware? Why do we need it?
+- How express JS basically handles requests behind the scenes
+- difference between app.use and app.app
+- Write a dummy auth middleware for admin
+- Write a dummy auth middleware for all user routes except /user/login
+- 
--- src/app.js
+++ src/app.js
@@ -2,36 +2,28 @@ const express = require('express');
 
 const app = express();
 
-// app.use("/route", rH, [rH2, rH3], rH4, rH5);
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user/login", (req, res) => {
+  res.send("User logged in successfully");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data sent");
+});
 
 
-// This will only from GET method
-app.get("/user",
-   [(req, res, next) => {
-      console.log("Handling the route user 1!");
-      next();
-      //res.send("Response!!");
-    },
-    (req, res, next) => {
-      console.log("Handling the route user 2!");
-      //res.send("2nd Response!!");
-      next();
-    }],
-    (req, res, next) => {
-      console.log("Handling the route user 3!");
-      //res.send("3nd Response!!");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Handling the route user 4!");
-      //res.send("4nd Response!!");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Handling the route user 5!");
-      res.send("5nd Response!!");
-    },
-);
+app.get("/admin/getAllData", (req, res) => {
+  // Logic of checking if the request is authorized
+    res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  // Logic of checking if the request is authorized
+  res.send("Deleted a user");
+});
 
 
 app.listen(7777, () => {
